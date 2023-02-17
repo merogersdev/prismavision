503b10d16c5b21344166d3e202e4bcee09480bb5
Michelle Rogers <michelle@merogers.dev>
2023-02-16 17:41:09 -0800

    add rtk-query

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,11 +1,14 @@
-import authReducer from "./features/authSlice";
-import { configureStore } from "@reduxjs/toolkit";
+import authReducer from '../features/authSlice';
+import { configureStore } from '@reduxjs/toolkit';
+
+//TODO createListenerMiddleware for localStorage parsing
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
   },
 });
+
 export type RootState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
